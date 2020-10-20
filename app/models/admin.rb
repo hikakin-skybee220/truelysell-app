09bd9a89abf6fd30fe20d0f2a85df000288ed088
HikaruShimada <hikarureihika@gmail.com>
2020-10-21 02:59:33 +0900

    Edit admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < ApplicationRecord
-    validates :email, {presence: true, uniqueness: true}
+    validates :name, {presence: true, uniqueness: true}
     validates :password, {presence: true}
 
     has_secure_password
