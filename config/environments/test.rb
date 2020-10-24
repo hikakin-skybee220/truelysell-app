f1f8b0aeeb816c2b2047d820ded614b763bb69a4
HikaruShimada <hikarureihika@gmail.com>
2020-10-25 02:16:19 +0900

    Edit test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -49,4 +49,16 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :test
     config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            'hikarureihika@gmail.com',
+    password:             'hikaru12202501',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
