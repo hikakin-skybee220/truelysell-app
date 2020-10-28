a0e6d11da891121ccc36b0d35c2eceff845fbe43
HikaruShimada <hikarureihika@gmail.com>
2020-10-28 11:09:15 +0900

    Error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,13 +110,14 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'truelysell-app.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'truelysell-app.herokuapp.com',
+  :domain => 'herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
