7e2fd7265beb9aa3a6798e6ae32ec985f1660055
HikaruShimada <hikarureihika@gmail.com>
2020-10-28 17:21:32 +0900

    Error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,7 +113,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'truelysell-app.herokuapp.com'
-  config_action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
